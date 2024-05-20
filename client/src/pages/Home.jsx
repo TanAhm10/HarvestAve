583de275e384ad0b306a6a1cf52408fe7939be1e
TanAhm10 <atanvir047@gmail.com>
2024-05-20 16:19:27 -0400

    firre

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import pumpkin from '../assets/pumpkin.png';
+import pumpkin2 from '../assets/pumpkin2.png';
 
 export default function Home() {
   return (
     <>
-    <div className="flex justify-center items-center min-h-screen">
-        <h1>Home</h1>
-    </div>
+      <div className="flex justify-center items-center min-h-screen relative">
+        <img
+          src={pumpkin}
+          alt="pumpkin"
+          className="w-1/6 h-auto"
+          style={{ position: 'absolute', bottom: '0', left: '0', margin: '0px' }}
+        />
+        <img
+          src={pumpkin2}
+          alt="pumpkin2"
+          className="w-1/6 h-auto"
+          style={{ position: 'absolute', bottom: '0', right: '0', margin: '0px' }} 
+        />
+      </div>
     </>
   );
 };
