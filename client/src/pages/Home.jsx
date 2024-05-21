cd1f761dd23fe4a8cece625f95c168f2e24bd9fe
Nayed Ali <naa23@njit.edu>
2024-05-21 00:46:13 -0400

    changed name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import pumpkin from '../assets/pumpkin.png';
 import pumpkin2 from '../assets/pumpkin2.png';
-//hello
+import pumpkin3 from '../assets/pumpkin3.png';
+import kodu from '../assets/kodu.png';
+import carrot from '../assets/carrot.png';
+import tomatoe from '../assets/tomatoes.png';
 
 export default function Home() {
   return (
     <>
       <div className="flex justify-center items-center min-h-screen relative">
+        <img
+          src={kodu}
+          alt="kodu"
+          className="w-1/6 h-auto"
+          style={{ position: 'absolute', bottom: '7%', left: '-7%', margin: '0px' }}
+        />
         <img
           src={pumpkin}
           alt="pumpkin"
@@ -14,11 +23,24 @@ export default function Home() {
           style={{ position: 'absolute', bottom: '0', left: '0', margin: '0px' }}
         />
         <img
+          src={carrot}
+          alt="carrot"
+          className="w-1/5 h-auto"
+          style={{ position: 'absolute', bottom: '-18%', left: '12%', margin: '0px' }}
+        />
+        <img
+          src={tomatoe}
+          alt="tomatoe"
+          className="w-1/5 h-auto"
+          style={{ position: 'absolute', bottom: '6%', right: '-5%', margin: '0px' }}
+        />
+        <img
           src={pumpkin2}
           alt="pumpkin2"
           className="w-1/6 h-auto"
           style={{ position: 'absolute', bottom: '0', right: '0', margin: '0px' }} 
         />
+
       </div>
     </>
   );
